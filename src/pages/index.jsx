a2a0f9ed31cc2f4fd69f0ea3c484d9291e55f185
Partha Priyadarshi Das <66919291+MrSnor@users.noreply.github.com>
2023-10-30 02:29:39 +0530

    add fileUpload component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import FileUpload from '@/components/FileUploadAndPrev/FileUpload'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -43,7 +44,7 @@ export default function Home() {
           priority
         />
       </div>
-
+      <FileUpload />
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
